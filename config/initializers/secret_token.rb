03b57c99d895e912d73c5706f131f6fbe0f6a94d
& Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-03-31 14:57:28 -0500

    creating routes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsCraigs::Application.config.secret_key_base = 'd591f70a351730647f6306945c41bf05873c5e70d1f0463be21a894992001960db26b3d4e05740fac07ba797f602037deff021148e9b03c2b6342c033b9216bf'
