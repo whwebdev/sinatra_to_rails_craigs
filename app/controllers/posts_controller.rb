45fbd691d35a4e78e90bde2222fe7db5fc1cfcc2
& Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-03-31 20:53:26 -0500

    can create new posts. editing coming soon.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,18 @@
 class PostsController < ApplicationController
   def new
+    @category = Category.find(params[:category_id])
+    @post = Post.new
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def create
+    post = Post.new(create_post_params)
+    post.category_id = params[:category_id]
+    post.save
+    redirect_to post
   end
 
   def check_key
@@ -19,4 +28,9 @@ class PostsController < ApplicationController
   def edit
   end
 
+  private
+
+  def create_post_params
+    params.require(:post).permit(:title, :body, :price, :email)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :category
+  before_create :generate_edit_url
+
+  def generate_edit_url
+    char_array = (1..9).to_a + ('A'..'Z').to_a + ('a'..'z').to_a
+    self.edit_url = char_array.sample(8).join
+  end
 end
